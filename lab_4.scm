05a6886136974f18d7a4372bb0dbb5f2faed2fc7
marcheanin <marcheanin1576@gmail.com>
2021-11-23 12:05:01 +0300

    finish lab_4

--- lab_4.scm
+++ lab_4.scm
@@ -24,12 +24,12 @@
              (begin
                (display "FAILED: ")
                (r (display (quote func)))
+               )
              )
          )
        )
      )
     )
-  )
   )
 
 (use-assertions)
@@ -91,11 +91,184 @@
             (if (or (eq? str #\newline) (eq? str #\return))
                 (read-loop (+ count 1))
                 (read-loop count)
+                )
             )
-        )
         )
       (read-loop 0)
       ))
   )
 
 (count-line "lab_4.scm")
+
+;#3
+(define (trib n)
+  (if (<= n 1)
+      0
+      (if (= n 2)
+          1
+          (+ (trib (- n 1)) (trib (- n 2)) (trib (- n 3)))
+          )
+      )
+  )
+
+(define memory '(((trib-memory 0) . 0)
+                 ((trib-memory 1) . 0)
+                 ((trib-memory 2) . 1)
+                 )
+  )
+
+(define (trib-memory n)
+  (or (letrec ((loop (lambda (m)
+                       (and (not (null? m))
+                            (if (equal? `(trib-memory ,n) (caar m))
+                                (cdar m)
+                                (loop (cdr m))
+                                )
+                            )
+                       )
+                     )
+               )
+        (loop memory))
+      (let ((rez (+ (trib-memory (- n 1))
+                    (trib-memory (- n 2))
+                    (trib-memory (- n 3))
+                    )))
+        (begin (set! memory (cons `((trib-memory ,n) . ,rez) memory))
+               rez)
+        )
+      )
+  )
+(trib-memory 10)
+
+;#4
+(define-syntax my-if
+  (syntax-rules ()
+    ((my-if condition if_t if_f)
+     (force (or (and condition
+                     (delay if_t))
+                (delay if_f)))
+     )
+    )
+  )
+(my-if #t 1 (/ 1 0))
+(my-if #f (/ 1 0) 1)
+
+;#5
+(define-syntax my-let
+  (syntax-rules ()
+    ((my-let ((value mean)) action)
+     ((lambda (value) action) mean))
+    ((my-let ((value mean) . assig) action)
+     (my-let assig ((lambda (value) action) mean)))
+    )
+  )
+
+(my-let ((x 2)) x)
+(my-let ((x 2) (y 3) (z 4)) (+ x y z))
+
+(define-syntax my-let*
+  (syntax-rules ()
+    ((my-let* ((value mean)) action)
+     ((lambda (value) action) mean))
+    ((my-let* ((value mean) . assig) action)
+     ((lambda (value) (my-let* assig action)) mean))
+    )
+  )
+
+(my-let* ((x 2) (y (+ x 2))) y)
+
+;#6
+
+;A
+(define-syntax when
+  (syntax-rules ()
+    ((when condition action) (and (quote condition) action))                         
+    ((when condition . actions) (and condition (begin . actions)))
+    )
+  )
+(define x 2)
+(when (> x 0) (display "x > 0")  (newline))
+
+(define-syntax unless
+  (syntax-rules ()
+    ((unless condition action) (if (not (quote condition)) action))                         
+    ((unless condition . actions) (if (not condition) (begin . actions)))
+    )
+  )
+(unless (= x 0) (display "x != 0") (newline))
+
+;Б
+(define-syntax for
+  (syntax-rules (in as)
+    ((for x in xs . actions) (letrec ((loop (lambda (xs1)
+                                              (if (not (null? xs1))
+                                                  (let ((x (car xs1)))
+                                                    (begin (begin . actions) (loop (cdr xs1))))
+                                                  )
+                                              )))
+                               (loop xs))
+                             )
+    ((for xs as x . actions) (for x in xs . actions))
+    )
+  )
+
+(for i in '(1 2 3)
+  (for j in '(4 5 6)
+    (display (list i j))
+    (newline)))
+
+(for '(1 2 3) as i
+  (for '(4 5 6) as j
+    (display (list i j))
+    (newline)))
+(newline)
+
+;B
+(define-syntax while
+  (syntax-rules ()
+    ((while condition . actions)
+     (letrec ((loop (lambda ()
+                      (if (and condition)
+                          (begin (begin . actions) (loop))
+                          )
+                      )
+                    )) (loop))
+     )
+    )
+  )
+(let ((p 0)
+      (q 0))
+  (while (< p 3)
+         (set! q 0)
+         (while (< q 3)
+                (display (list p q))
+                (newline)
+                (set! q (+ q 1)))
+         (set! p (+ p 1))))
+
+(newline)
+;Г
+(define-syntax repeat
+  (syntax-rules (until)
+    ((repeat actions until condition) (letrec ((loop (lambda ()
+                                                       (begin (begin . actions)
+                                                              (if (not (and condition))
+                                                                  (loop)
+                                                                  )
+                                                              )
+                                                       ))
+                                               ) (loop)) 
+                                      )
+    )
+  )
+(let ((i 0)
+      (j 0))
+  (repeat ((set! j 0)
+           (repeat ((display (list i j))
+                    (set! j (+ j 1)))
+                   until (= j 3))
+           (set! i (+ i 1))
+           (newline))
+          until (= i 3)))
+
+;Д
