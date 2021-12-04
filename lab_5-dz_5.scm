29a5a5097208fbacd1bdc5a52e6175fa776a36b6
marcheanin <marcheanin1576@gmail.com>
2021-12-04 22:21:48 +0300

    finish funcs of interpretator, that are needed in lab

---
+++ lab_5-dz_5.scm
@@ -0,0 +1,212 @@
+(load "unit-test.scm")
+
+(define (interpret program stack)
+  (letrec
+      ((return-stack stack)
+       (words '())
+       (varaibles '())
+       (put1 (lambda (x)
+               (set! stack (cons x stack))
+               ))
+       (peak1 (lambda ()
+                (car stack)))
+       (peak2 (lambda ()
+                (if (null? (car stack))
+                    (cadr stack)
+                    (if (null? (cdar stack))
+                        (list (caar stack))
+                        (list (caar stack) (cadar stack)))))
+              )
+       (format-logic (lambda (a)
+                       (if a -1 0)))
+       (op= (lambda (a b)
+              (format-logic (= a b))))
+       (op< (lambda (a b)
+              (format-logic (< a b))))
+       (op> (lambda (a b)
+              (format-logic (> a b))))
+       (true? (lambda (a)
+                (not (= a 0))))
+       (op-not (lambda (a)
+                 (format-logic (not (true? a)))))
+       (op-and (lambda (a b)
+                 (format-logic (and (true? a) (true? b)))))
+       (op-or (lambda (a b)
+                (format-logic (or (true? a) (true? b)))))
+       
+       (skip (lambda (i balance beg end)
+               (if (or
+                    (>= i (vector-length program))
+                    (= balance 0))
+                   (- i 1)
+                   (begin
+                     (if (equal? (vector-ref program i) end) (set! balance (- balance 1)))
+                     (if (equal? (vector-ref program i) beg) (set! balance (+ balance 1)))
+                     (skip (+ 1 i) balance beg end)))))
+       (skip-if (lambda (i balance)
+                  (if (or
+                       (>= i (vector-length program))
+                       (= balance 0))
+                      (- i 1)
+                      (begin
+                        (if (equal? (vector-ref program i) 'endif) (set! balance (- balance 1)))
+                        (if (and (= balance 1) (equal? (vector-ref program i) 'else)) (set! balance (- balance 1)))
+                        (if (equal? (vector-ref program i) 'if) (set! balance (+ balance 1)))
+                        (skip-if (+ 1 i) balance)))))
+       
+       (interpret-symbol (lambda (i)
+                           (if (< i (vector-length program))
+                               (let ((symbol (vector-ref program i)))
+                                 (cond
+                                   ;number
+                                   ((number? symbol) (put1 symbol))
+
+                                   ;arithmetic
+                                   ((equal? symbol '+) (set! stack (cons (+ (cadr stack) (car stack)) (cddr stack))))
+                                   ((equal? symbol '-) (set! stack (cons (- (cadr stack) (car stack)) (cddr stack))))
+                                   ((equal? symbol '*) (set! stack (cons (* (cadr stack) (car stack)) (cddr stack))))
+                                   ((equal? symbol '/) (set! stack (cons (/ (cadr stack) (car stack)) (cddr stack))))
+                                   ((equal? symbol 'mod) (set! stack (cons (remainder (cadr stack) (car stack)) (cddr stack))))
+                                   ((equal? symbol 'neg) (set! stack (cons (- (car stack)) (cdr stack))))
+
+                                   ;compare
+                                   ((equal? symbol '=) (set! stack (cons (op= (cadr stack) (car stack)) (cddr stack))))
+                                   ((equal? symbol '>) (set! stack (cons (op> (cadr stack) (car stack)) (cddr stack))))
+                                   ((equal? symbol '<) (set! stack (cons (op< (cadr stack) (car stack)) (cddr stack))))
+
+                                   ;logic
+                                   ((equal? symbol 'not) (set! stack (cons (op-not (car stack)) (cdr stack))))
+                                   ((equal? symbol 'and) (set! stack (cons (op-and (cadr stack) (car stack)) (cddr stack))))
+                                   ((equal? symbol 'or) (set! stack (cons (op-or (cadr stack) (car stack)) (cddr stack))))
+
+                                   ;stack operations
+                                   ((equal? symbol 'drop) (set! stack (cdr stack)))
+                                   ((equal? symbol 'swap) (set! stack (cons (cadr stack) (cons (car stack) (cddr stack)))))
+                                   ((equal? symbol 'dup) (set! stack (cons (car stack) stack)))
+                                   ((equal? symbol 'over) (set! stack (cons (cadr stack) stack)))
+                                   ((equal? symbol 'rot) (set! stack (cons (caddr stack) (cons (cadr stack) (cons (car stack) (cdddr stack))))))
+                                   ((equal? symbol 'depth) (set! stack (cons (length stack) stack)))
+
+                                   ;if
+                                   ((equal? symbol 'if)
+                                    (begin
+                                      (if (not (true? (car stack))) (set! i (skip-if (+ 1 i) 1)))
+                                      (set! stack (cdr stack))))
+                                   ((equal? symbol 'else) (set! i (skip-if (+ 1 i) 1)))
+
+                                   ;define
+                                   ((equal? symbol 'define)
+                                    (begin
+                                      (set! i (+ 1 i))
+                                      (set! words (cons (list (vector-ref program i) i) words))
+                                      (set! i (skip (+ 1 i) 1 'define 'end))))
+                                   ((assq symbol words)
+                                    (begin
+                                      (set! return-stack (cons i return-stack))
+                                      (set! i (cadr (assq symbol words)))))
+                                   ((or (equal? symbol 'end) (equal? symbol 'exit) (equal? symbol 'endlambda))
+                                    (begin
+                                      (set! i (car return-stack))
+                                      (set! return-stack (cdr return-stack))
+                                      (if (equal? symbol 'endlambda)
+                                          (begin (set! i (car return-stack))
+                                                 (set! return-stack (cdr return-stack))))))
+                                   )
+                                 
+                                   
+                                 ;(display stack) (newline)  
+                                 (interpret-symbol (+ i 1)))                          
+                               )
+                           )
+                         )
+       )
+    (begin
+      (interpret-symbol 0)
+      stack))
+  )
+
+(define tests (list
+               (test (interpret #(2 3 * 4 5 * +) '()) '(26))
+               (test (interpret #(neg) '(1)) '(-1))
+               (test (interpret #(10 2 /) '()) '(5))
+               (test (interpret #(10 2 >) '()) '(-1))
+               (test (interpret #(10 2 <) '()) '(0))
+               (test (interpret #(10 5 + 15 =) '()) '(-1))
+               (test (interpret #(1 1 and) '()) '(-1))
+               (test (interpret #(1 0 and) '()) '(0))
+               (test (interpret #(1 0 or) '()) '(-1))
+               (test (interpret #(1 2 3 drop) '()) '(2 1))
+               (test (interpret #(1 2 3 4 5 swap) '()) '(4 5 3 2 1))
+               (test (interpret #(1 2 3 4 5 dup) '()) '(5 5 4 3 2 1))
+               (test (interpret #(1 2 3 4 5 over) '()) '(4 5 4 3 2 1))
+               (test (interpret #(1 2 3 4 5 rot) '()) '(3 4 5 2 1))
+               (test (interpret #(depth 0 0 0 depth 0 0 0 depth) '()) '(8 0 0 0 4 0 0 0 0))
+               (test (interpret #(1 dup if dup dup if 5 endif endif 3) '()) '(3 5 1 1))
+               (test (interpret #(0 dup if dup dup if 5 endif endif 3) '()) '(3 0))
+               (test (interpret #(   define abs 
+                                      dup 0 < 
+                                      if neg endif 
+                                      end 
+                                      abs    ) 
+                                '(-9)) '(9))
+               (test (interpret #(   define =0? dup 0 = end
+                                      define <0? dup 0 < end
+                                      define signum
+                                      =0? if exit endif
+                                      <0? if drop -1 exit endif
+                                      drop
+                                      1
+                                      end
+                                      0 signum
+                                      -5 signum
+                                      10 signum       ) (quote ())) '(1 -1 0))
+               (test (interpret #(   define -- 1 - end
+                                      define =0? dup 0 = end
+                                      define =1? dup 1 = end
+                                      define factorial
+                                      =0? if drop 1 exit endif
+                                      =1? if drop 1 exit endif
+                                      dup --
+                                      factorial
+                                      *
+                                      end
+                                      0 factorial
+                                      1 factorial
+                                      2 factorial
+                                      3 factorial
+                                      4 factorial     ) (quote ())) '(24 6 2 1 1))
+               (test (interpret #(   define =0? dup 0 = end
+                                      define =1? dup 1 = end
+                                      define -- 1 - end
+                                      define fib
+                                      =0? if drop 0 exit endif
+                                      =1? if drop 1 exit endif
+                                      -- dup
+                                      -- fib
+                                      swap fib
+                                      +
+                                      end
+                                      define make-fib
+                                      dup 0 < if drop exit endif
+                                      dup fib
+                                      swap --
+                                      make-fib
+                                      end
+                                      10 make-fib     ) (quote ())) '(0 1 1 2 3 5 8 13 21 34 55))
+               (test (interpret #(   define =0? dup 0 = end
+                                      define gcd
+                                      =0? if drop exit endif
+                                      swap over mod
+                                      gcd
+                                      end
+                                      90 99 gcd
+                                      234 8100 gcd    ) '()) '(18 9))
+               )
+  
+  )
+
+(run-tests tests)
+
+               
+
+                           
