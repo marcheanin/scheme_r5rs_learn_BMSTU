a9f805c8cd8bb74ab2aede3bab5c5c394bb04d51
marcheanin <marcheanin1576@gmail.com>
2021-12-10 22:24:25 +0300

    finish dz features

--- lab_5-dz_5.scm
+++ lab_5-dz_5.scm
@@ -1,5 +1,11 @@
 (load "unit-test.scm")
 
+'feature-if-else
+'feature-nested-if
+'feature-repeat-loop
+'feature-break-continue
+'feature-switch-case
+
 (define (interpret program stack)
   (letrec
       ((return-stack stack)
@@ -9,13 +15,13 @@
                (set! stack (cons x stack))
                ))
        (peak1 (lambda ()
-                (car stack)))
+                (car stack))) ;not used
        (peak2 (lambda ()
                 (if (null? (car stack))
                     (cadr stack)
                     (if (null? (cdar stack))
                         (list (caar stack))
-                        (list (caar stack) (cadar stack)))))
+                        (list (caar stack) (cadar stack))))) ;not used
               )
        (format-logic (lambda (a)
                        (if a -1 0)))
@@ -53,6 +59,18 @@
                         (if (and (= balance 1) (equal? (vector-ref program i) 'else)) (set! balance (- balance 1)))
                         (if (equal? (vector-ref program i) 'if) (set! balance (+ balance 1)))
                         (skip-if (+ 1 i) balance)))))
+       (skip-switch (lambda (i case)
+                          (if (>= i (vector-length program))
+                              (- i 1)
+                              (if (or (equal? (vector-ref program i) 'endswitch)
+                                      (and (equal? (vector-ref program i) 'case)
+                                           (equal? (vector-ref program (+ i 1)) case)))
+                                  (if (equal? (vector-ref program i) 'endswitch)
+                                      i
+                                      (+ 1 i))
+                                  (if (equal? (vector-ref program i) 'switch)
+                                      ( + 1 (skip (+ 1 i) 1 'switch 'endswitch))
+                                      (skip-switch (+ 1 i) case))))))
        
        (interpret-symbol (lambda (i)
                            (if (< i (vector-length program))
@@ -65,7 +83,7 @@
                                    ((equal? symbol '+) (set! stack (cons (+ (cadr stack) (car stack)) (cddr stack))))
                                    ((equal? symbol '-) (set! stack (cons (- (cadr stack) (car stack)) (cddr stack))))
                                    ((equal? symbol '*) (set! stack (cons (* (cadr stack) (car stack)) (cddr stack))))
-                                   ((equal? symbol '/) (set! stack (cons (/ (cadr stack) (car stack)) (cddr stack))))
+                                   ((equal? symbol '/) (set! stack (cons (quotient (cadr stack) (car stack)) (cddr stack))))
                                    ((equal? symbol 'mod) (set! stack (cons (remainder (cadr stack) (car stack)) (cddr stack))))
                                    ((equal? symbol 'neg) (set! stack (cons (- (car stack)) (cdr stack))))
 
@@ -111,6 +129,46 @@
                                       (if (equal? symbol 'endlambda)
                                           (begin (set! i (car return-stack))
                                                  (set! return-stack (cdr return-stack))))))
+
+                                   ;untill
+                                   ((equal? symbol 'repeat) (begin
+                                                              (if (not (true? (car stack)))
+                                                                  (begin
+                                                                    (set! return-stack
+                                                                          (cons
+                                                                           (+ 1 (skip (+ 1 i) 1 'repeat 'until))
+                                                                           return-stack)))
+                                                                  (begin
+                                                                    (set! return-stack (cons i return-stack))))))
+                                   ((or (equal? symbol 'until))
+                                    (begin
+                                      (if (equal? i (- (car return-stack) 1))
+                                          (set! stack (cdr stack)))
+                                      (set! i (- (car return-stack) 1))                                          
+                                      (set! return-stack (cdr return-stack))))
+                                   ;continue
+                                   ((or (equal? symbol 'continue))
+                                    (begin
+                                      (set! i (- (car return-stack) 1))
+                                      (if (equal? (vector-ref program i) 'until)
+                                          (set! stack (cdr stack)))
+                                      (if (not (equal? (vector-ref program i) 'lambda))
+                                          (set! return-stack (cdr return-stack)))))
+
+                                   ;break
+                                   ((or (equal? symbol 'break))
+                                    (begin
+                                      (set! stack (cons 0 (cdr stack)))
+                                      (set! i (- (car return-stack) 1))
+                                      (set! return-stack (cdr return-stack))))
+                                   ;switch
+                                   ((equal? symbol 'switch)
+                                        (begin
+                                          (set! i (skip-switch (+ i 1) (car stack)))
+                                          (set! stack (cdr stack))
+                                          ))
+                                       ((equal? symbol 'exitcase)
+                                        (set! i (skip (+ i 1) 1 'switch 'endswitch)))
                                    )
                                  
                                    
@@ -201,11 +259,39 @@
                                       end
                                       90 99 gcd
                                       234 8100 gcd    ) '()) '(18 9))
+               (test (interpret #(1 if 100 else 200 endif) '()) '(100))
+               (test (interpret #(0 if 100 else 200 endif) '()) '(200))
+               (test (interpret #(0 if 1 if 2 endif 3 endif 4) '()) '(4))
+               (test (interpret #(1 if 2 if 3 endif 4 endif 5) '()) '(5 4 3))
+               (test (interpret #(1 if 0 if 2 endif 3 endif 4) '()) '(4 3))
+               (test (interpret #(3 repeat 1 swap 1 - until) '()) '(1 1 1 1))
+               (test (interpret #(2 10 repeat 1 - dup 2 mod if continue endif swap 2 * swap until) '()) '(64))
+               (test (interpret #(2 8 repeat 1 - dup 2 mod not if continue endif swap 2 * swap until) '()) '(64))
+               (test (interpret #(dup 3 mod
+                                      switch
+                                      case 0 0 exitcase
+                                      case 1 1 exitcase
+                                      case 2
+                                      switch
+                                      case 2 0 exitcase
+                                      endswitch
+                                      endswitch
+                                      ) '(17)) '())
+               (test (interpret #(dup 3 mod
+                                      switch
+                                      case 0 0 exitcase
+                                      case 1 1 exitcase
+                                      case 2
+                                      switch
+                                      case 2 0 exitcase
+                                      endswitch
+                                      endswitch
+                                      ) '(2)) '(0))
                )
-  
   )
 
 (run-tests tests)
+      
 
                
 
