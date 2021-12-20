29fd70669399bc111483f93151b9f74f35686ec3
marcheanin <marcheanin1576@gmail.com>
2021-12-20 09:15:47 +0300

    finish #6 lab

--- lab_6.scm
+++ lab_6.scm
@@ -1,5 +1,6 @@
 (load "unit-test.scm")
 ;(load "lab_3.rkt")
+(load "dz_4.scm")
 
 ;#1
 
@@ -139,18 +140,18 @@
                            (if (null? xs)
                                fracs
                                (loop (cdr xs) fracs))
-                       )
-                   )))
+                           )
+                       )))
            (try-frac (lambda (xs)
                        (letrec ((loop2 (lambda (xs1 xs2 res)
-                                        (if (null? xs1)
-                                            res
-                                            (begin (set! xs2 (append xs2 (list (car xs1))))
-                                                   (if (frac xs2)
-                                                       (set! res xs2))
-                                                   (loop2 (cdr xs1) xs2 res))
-                                            )
-                                        )))
+                                         (if (null? xs1)
+                                             res
+                                             (begin (set! xs2 (append xs2 (list (car xs1))))
+                                                    (if (frac xs2)
+                                                        (set! res xs2))
+                                                    (loop2 (cdr xs1) xs2 res))
+                                             )
+                                         )))
                          (loop2 xs '() '())
                          )
                        ))
@@ -167,12 +168,8 @@
         (complete-fracs list-fracs '())
         )
     )
-  )
-
-                       
-                                     
-                                     
-
+  )                                     
+                                   
 (define (check-frac str)
   (if (frac (make-stream str))
       #t
@@ -192,28 +189,171 @@
 
 
 (define tests (list
-        (test (check-frac "110/111") #t)
-        (test (check-frac "-4/3") #t)    
-        (test (check-frac "+5/10") #t)   
-        (test (check-frac "5.0/10") #f)
-        (test (check-frac "FF/10") #f)
-        (test (check-frac "/313") #f)
-        (test (check-frac "-2a3/23") #f)
+               (test (check-frac "110/111") #t)
+               (test (check-frac "-4/3") #t)    
+               (test (check-frac "+5/10") #t)   
+               (test (check-frac "5.0/10") #f)
+               (test (check-frac "FF/10") #f)
+               (test (check-frac "/313") #f)
+               (test (check-frac "-2a3/23") #f)
 
-        (test (scan-frac "110/111") 110/111)
-        (test (scan-frac "-4/3") -4/3)
-        (test (scan-frac "+5/10") 1/2)
-        (test (scan-frac "5.0/10") #f)
-        (test (scan-frac "FF/10") #f)
+               (test (scan-frac "110/111") 110/111)
+               (test (scan-frac "-4/3") -4/3)
+               (test (scan-frac "+5/10") 1/2)
+               (test (scan-frac "5.0/10") #f)
+               (test (scan-frac "FF/10") #f)
 
-        (test (scan-many-fracs "111/1233 11/222 -2/23") '(111/1233 11/222 -2/23))
-        (test (scan-many-fracs "\t1/2 1/3\n\n10/8") '(1/2 1/3 5/4))
-        (test (scan-many-fracs "\t1/2 1/3\n\n2/-5") #f)
-        )
-       )
+               (test (scan-many-fracs "111/1233 11/222 -2/23") '(111/1233 11/222 -2/23))
+               (test (scan-many-fracs "\t1/2 1/3\n\n10/8") '(1/2 1/3 5/4))
+               (test (scan-many-fracs "\t1/2 1/3\n\n2/-5") #f)
+               )
+  )
 (run-tests tests)
 
 ;#2
+
+;структура с основными операциями
+(define-struct stream (symbols))
+(define (stream-peek stream)
+  (and
+   (pair? (stream-symbols stream))
+   (let ((symbol (car (stream-symbols stream))))
+     symbol)))
+(define (stream-next stream)
+  (let ((symbol (car (stream-symbols stream))))
+    (set-stream-symbols! stream (cdr (stream-symbols stream)))
+    symbol))
+(define (string->stream str)
+  (make-stream (string->list (string-append str (string finish-symbol)))))
+
+
+(define (vector->stream vec)
+  (make-stream (append (vector->list vec) (list finish-symbol))))
+
+(define (parse vec)
+  (letrec (
+           (stream (vector->stream vec))
+           (forbidden-words (map list (list finish-symbol 'endif 'end)))
+           ; finish-symbol
+           (parse-finish-symbol (lambda ()
+                                  (let ((finish (stream-peek stream)))
+                                    (and (equal? finish finish-symbol)
+                                         (begin (stream-next stream) finish))))
+                                )
+           ; <Program>  ::= <Articles> <Body> .
+           (parse-program (lambda ()
+                            (let ((articles (parse-articles))
+                                  (body (parse-body))
+                                  (finish (parse-finish-symbol)))
+                              (and (and articles body finish)
+                                   (list articles body))))
+                          )
+           ; <Articles> ::= <Article> <Articles> | .
+           (parse-articles (lambda ()
+                             (let* ((article (parse-article))
+                                    (articles (and
+                                               (not (equal? article '()))
+                                               article
+                                               (parse-articles))))
+                               (if (and article articles (not (equal? article '())))
+                                   (cons article articles)
+                                   article)))
+                           )
+           ; define
+           (parse-define (lambda ()
+                           (let ((def (stream-peek stream)))
+                             (and (equal? def 'define)
+                                  (begin (stream-next stream) def))))
+                         )
+           ; end
+           (parse-end (lambda ()
+                        (let ((end (stream-peek stream)))
+                          (and (equal? end 'end)
+                               (begin (stream-next stream) end)))))
+           ; word
+           (parse-word (lambda ()
+                         (let ((word (stream-peek stream)))
+                           (begin
+                             (stream-next stream)
+                             word)))
+                       )
+           ; <Article>  ::= define word <Body> end .
+           (parse-article (lambda ()
+                            (let* ((def (parse-define))
+                                   (word (and def (parse-word)))
+                                   (body (and word (parse-body)))
+                                   (end (and body (parse-end))))
+                              (if def
+                                  (and def word body end
+                                       (list word body))
+                                  '())
+                              )
+                            )
+                          )
+           ; endif
+           (parse-endif (lambda ()
+                          (let ((endif (stream-peek stream)))
+                            (and (equal? endif 'endif)
+                                 (begin (stream-next stream) endif)))))
+           ; <Body> ::= if <Body> endif <Body> | integer <Body> | word <Body> | .
+           (parse-body (lambda ()
+                         (let ((first (stream-peek stream)))
+                           (cond
+                             ; if <Body> endif <Body>
+                             ((equal? first 'if)
+                              (stream-next stream)
+                              (let* ((body (parse-body))
+                                     (endif (and body (parse-endif)))
+                                     (body-next (and endif (parse-body))))
+                                (and first body endif body-next
+                                     (append (list (append (list first body)))  body-next))))
+                             ; integer <Body>
+                             ((number? first)
+                              (stream-next stream)
+                              (let ((body (parse-body)))
+                                (and first body
+                                     (cons first body))))
+                             ; word <Body>
+                             ((not (assq first forbidden-words))
+                              (stream-next stream)
+                              (let ((body (parse-body)))
+                                (and first body
+                                     (cons first body))))
+                             ; else
+                             (else '())
+                             )))))
+    (parse-program)))
+
+
+
+(define tests (list
+               (test (parse #(1 2 +)) '(() (1 2 +)))
+               (test (parse #(x dup 0 swap if drop -1 endif)) '(() (x dup 0 swap (if (drop -1)))))
+               (test (parse #( define -- 1 - end
+                                define =0? dup 0 = end
+                                define =1? dup 1 = end
+                                define factorial
+                                =0? if drop 1 exit endif
+                                =1? if drop 1 exit endif
+                                dup --
+                                factorial
+                                *
+                                end
+                                0 factorial
+                                1 factorial
+                                2 factorial
+                                3 factorial
+                                4 factorial )) '(((-- (1 -))
+                                                  (=0? (dup 0 =))
+                                                  (=1? (dup 1 =))
+                                                  (factorial
+                                                   (=0? (if (drop 1 exit)) =1? (if (drop 1 exit)) dup -- factorial *)))
+                                                 (0 factorial 1 factorial 2 factorial 3 factorial 4 factorial)))
+               (test (parse #(if 1 2 endif if 3 4)) #f)
+               (test (parse #(define word w1 w2 w3)) #f)
+               ))
+
+(run-tests tests)
            
   
       
